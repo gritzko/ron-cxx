c2685c360551eb203806ebdccfbd43b21e6ec298
Victor Grishchenko <gritzko@pm.me>
2018-12-30 17:33:18 +0300

    mapper: op

--- db/map/map.hpp
+++ db/map/map.hpp
@@ -27,20 +27,43 @@ namespace ron {
     static const Uuid CHAIN_MAPPER_ID{715112314629521408UL,0};
 
     template<typename Frame>
+    struct OpMapper {
+        typedef typename Frame::Cursor Cursor;
+        typedef typename Frame::Builder Builder;
+        typedef Replica<Frame> HostReplica;
+
+        HostReplica* host_;
+
+        OpMapper(HostReplica* host) : host_{host} {}
+
+        Status Map(Builder& response, Cursor& query) const;
+
+        Status Write(rocksdb::WriteBatch& batch, Cursor& query) const {
+            return Status::NOT_IMPLEMENTED;
+        }
+
+    };
+
+    static const Uuid OP_MAPPER_ID{933371022772535296UL,0};
+
+    template<typename Frame>
     struct MasterMapper {
         typedef typename Frame::Cursor Cursor;
         typedef typename Frame::Builder Builder;
         typedef Replica<Frame> HostReplica;
 
         ChainMapper<Frame> chain_;
+        OpMapper<Frame> op_;
 
-        MasterMapper(HostReplica* host) : chain_{host} {}
+        MasterMapper(HostReplica* host) : chain_{host}, op_{host} {}
 
         /** Map RON events into some external representation (e.g. JSON). */
         Status Map(Builder& response, Cursor& query) const {
             const Uuid& id = query.id();
             if (id == CHAIN_MAPPER_ID) {
                 return chain_.Map(response, query);
+            } else if (id == OP_MAPPER_ID) {
+                return op_.Map(response, query);
             } else {
                 return Status::NOT_IMPLEMENTED.comment("unknown mapper");
             }
---
+++ db/map/op.cc
@@ -0,0 +1,25 @@
+#include "map.hpp"
+
+namespace ron {
+
+    template<typename Frame>
+    Status OpMapper<Frame>::Map(Builder& response, Cursor& query) const {
+        const Uuid& ref = query.ref();
+        if (ref.version()!=TIME) return Status::BADARGS.comment("need an op id");
+        std::unique_ptr<rocksdb::Iterator> i{host_->FindChain(ref)};
+        if (i==nullptr) return Status::NOT_FOUND.comment("no such chain");
+        slice_t chain{slice(i->value())};
+        Cursor cc{chain};
+        while (cc.valid()) {
+            if (cc.id()==ref) {
+                response.AppendOp(cc); // TODO TERM
+                return Status::OK;
+            }
+            cc.Next();
+        }
+        return Status::NOT_FOUND.comment("no such op");
+    }
+
+    template class OpMapper<TextFrame>;
+
+}

5b32876e52e953729d37bd31021345cc1a821b8f
Victor Grishchenko <gritzko@pm.me>
2019-01-10 16:00:14 +0300

    some of us forget 'git add' way too often

---
+++ rdt/const.hpp
@@ -0,0 +1,56 @@
+#ifndef CPP_RDT_CONST_HPP
+#define CPP_RDT_CONST_HPP
+
+namespace ron {
+
+enum RDT : uint8_t {
+    META_RDT,
+    CHAIN_RDT,
+    ACK_RDT,
+    LWW_RDT,
+    RGA_RDT,
+    MX_RDT,
+    RDT_COUNT
+};
+
+static const uint64_t META_RDT_NAME{894494834235015168UL};
+static const Uuid META_RDT_ID{META_RDT_NAME, 0};
+static const uint64_t CHAIN_RDT_NAME{715112314629521408UL};
+static const Uuid CHAIN_RDT_ID{CHAIN_RDT_NAME, 0};
+static const uint64_t ACK_RDT_NAME{677716977128570880UL};
+static const Uuid ACK_RDT_ID{ACK_RDT_NAME, 0};
+static const uint64_t LWW_RDT_NAME{881557636825219072UL};
+static const Uuid LWW_RDT_ID{LWW_RDT_NAME, 0};
+static const uint64_t RGA_RDT_NAME{985043671231496192UL};
+static const Uuid RGA_RDT_ID{RGA_RDT_NAME, 0};
+static const uint64_t MX_RDT_NAME{899594025567256576UL};
+static const Uuid MX_RDT_ID{MX_RDT_NAME, 0};
+
+static const Uuid RDT_IDS[] = {META_RDT_ID, CHAIN_RDT_ID, ACK_RDT_ID,
+                               LWW_RDT_ID,  RGA_RDT_ID,   MX_RDT_ID,
+                               Uuid::FATAL};
+
+inline Uuid rdt2uuid(RDT e) { return RDT_IDS[e]; }
+
+inline RDT uuid2rdt(const Uuid& u) {
+    switch (uint8_t(u.value()._64 >> 52)) {
+        case uint8_t(META_RDT_NAME >> 52):
+            return u == META_RDT_ID ? META_RDT : RDT_COUNT;
+        case uint8_t(CHAIN_RDT_NAME >> 52):
+            return u == CHAIN_RDT_ID ? CHAIN_RDT : RDT_COUNT;
+        case uint8_t(ACK_RDT_NAME >> 52):
+            return u == ACK_RDT_ID ? ACK_RDT : RDT_COUNT;
+        case uint8_t(LWW_RDT_NAME >> 52):
+            return u == LWW_RDT_ID ? LWW_RDT : RDT_COUNT;
+        case uint8_t(RGA_RDT_NAME >> 52):
+            return u == RGA_RDT_ID ? RGA_RDT : RDT_COUNT;
+        case uint8_t(MX_RDT_NAME >> 52):
+            return u == MX_RDT_ID ? MX_RDT : RDT_COUNT;
+        default:
+            return RDT_COUNT;
+    }
+}
+
+}  // namespace ron
+
+#endif  // CPP_CONST_HPP
---
+++ rdt/mx.hpp
@@ -0,0 +1,76 @@
+#ifndef rdt_mx_hpp
+#define rdt_mx_hpp
+
+#include <bits/unordered_map.h>
+#include <unordered_map>
+#include "rdt/merge.hpp"
+#include "ron/status.hpp"
+
+namespace ron {
+
+typedef std::pair<fsize_t, fsize_t> mxidx_t;
+
+const mxidx_t MX_IDX_MAX{FSIZE_MAX, FSIZE_MAX};
+
+}  // namespace ron
+
+namespace std {
+
+template <>
+struct hash<ron::mxidx_t> {
+    size_t operator()(const ron::mxidx_t &idx) const noexcept {
+        return hash<int>()(idx.first) ^ hash<int>()(idx.second);
+    }
+};
+}  // namespace std
+
+namespace ron {
+
+template <typename Cursor>
+mxidx_t readmxidx(const Cursor &read) {
+    if (read.size() != 2 + 3 || !read.has(2, INT) || !read.has(3, INT))
+        return MX_IDX_MAX;
+    return mxidx_t{(fsize_t)read.integer(2), (fsize_t)read.integer(3)};
+}
+
+template <class Frame>
+class MatrixRDT {
+    static bool less_than(const Op &a, const Op &b) { return a.id() < b.id(); }
+
+    typedef MergeCursor<Frame, less_than> merger;
+    typedef typename Frame::Builder Builder;
+    typedef typename Frame::Cursor Cursor;
+    typedef typename Frame::Cursors Cursors;
+
+   public:
+    typedef std::unordered_map<mxidx_t, Atom> mx_t;
+
+    Status Merge(typename Frame::Builder &output, const Cursors &inputs) const {
+        merger m{inputs};
+        m.Merge(output);
+        return Status::OK;
+    }
+
+    Status GC(Builder &output, const Frame &input) const {
+        mx_t max{};
+        for (Cursor read = input.cursor(); read.valid(); read.Next()) {
+            mxidx_t at{read};
+            if (at == MX_IDX_MAX) continue;
+            max[at] = read.id();
+        }
+        for (Cursor write = input.cursor(); write.valid(); write.Next()) {
+            mxidx_t at{write};
+            if (at != MX_IDX_MAX && max[at] == write.id())
+                output.AppendOp(write);
+        }
+        return Status::OK;
+    }
+
+    Status MergeGC(Builder &output, const Cursors &inputs) const {
+        return Status::NOT_IMPLEMENTED;
+    }
+};
+
+}  // namespace ron
+
+#endif

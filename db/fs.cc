15b124319740b45fda618355189e243f6380df8a
Victor Grishchenko <gritzko@pm.me>
2019-01-30 18:39:00 +0300

    refac: fs functions to a separate file

---
+++ db/fs.cc
@@ -0,0 +1,38 @@
+#include <fcntl.h>
+#include <gflags/gflags.h>
+#include <unistd.h>
+#include <ctime>
+#include <algorithm>
+#include "ron/ron.hpp"
+
+using namespace std;
+using namespace ron;
+
+struct MPS {
+    bool operator()(char ch) const { return ch == '\\' || ch == '/'; }
+};
+string basename(string const& pathname) {
+    return string(find_if(pathname.rbegin(), pathname.rend(), MPS()).base(),
+                  pathname.end());
+}
+static int rmFiles(const char* pathname, const struct stat* sbuf, int type,
+                   struct FTW* ftwb) {
+    if (remove(pathname) < 0) {
+        perror("ERROR: remove");
+        return -1;
+    }
+    return 0;
+}
+#include <ftw.h>
+
+Status rm_dir(string path) {
+    if (nftw(path.c_str(), rmFiles, 10, FTW_DEPTH | FTW_MOUNT | FTW_PHYS) < 0) {
+        return Status::IOFAIL.comment("can't rmdir");
+    }
+    return Status::OK;
+}
+
+bool file_exists(const std::string& name) {
+    struct stat buffer;
+    return (stat(name.c_str(), &buffer) == 0);
+}
--- db/main.cc
+++ db/main.cc
@@ -48,36 +48,11 @@ Status CommandQuery(RonReplica& replica, const string& name);
 Status CommandDump(RonReplica& replica, const string& prefix);
 Status CommandTest(RonReplica& replica, const string& file);
 
+string basename(string const& pathname) ;
+Status rm_dir(string path);
+bool file_exists(const std::string& name);
 Status CompareFrames(const Frame& a, const Frame& b);
 
-struct MPS {
-    bool operator()(char ch) const { return ch == '\\' || ch == '/'; }
-};
-string basename(string const& pathname) {
-    return string(find_if(pathname.rbegin(), pathname.rend(), MPS()).base(),
-                  pathname.end());
-}
-static int rmFiles(const char* pathname, const struct stat* sbuf, int type,
-                   struct FTW* ftwb) {
-    if (remove(pathname) < 0) {
-        perror("ERROR: remove");
-        return -1;
-    }
-    return 0;
-}
-#include <ftw.h>
-
-Status rm_dir(string path) {
-    if (nftw(path.c_str(), rmFiles, 10, FTW_DEPTH | FTW_MOUNT | FTW_PHYS) < 0) {
-        return Status::IOFAIL.comment("can't rmdir");
-    }
-    return Status::OK;
-}
-
-inline bool file_exists(const std::string& name) {
-    struct stat buffer;
-    return (stat(name.c_str(), &buffer) == 0);
-}
 
 Status RunCommands() {
     RonReplica replica{};

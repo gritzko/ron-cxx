c27412a1ce8c744ba8893adf48baf0145d7da402
Victor Grishchenko <gritzko@pm.me>
2019-03-03 19:45:06 +0300

    extend the codegen script

--- bin/ragel.sh
+++ bin/ragel.sh
@@ -3,3 +3,9 @@
 ragel -C -G2 -e ragel/uuid-parser.rl -o ron/uuid-parser.cc
 ragel -C -G2 -e ragel/text-parser.rl -o ron/text-parser.cc
 ragel -C -G2 -e ragel/string-parser.rl -o ron/string-parser.cc
+if [ ! -d build ]; then
+    mkdir build;
+fi
+cd build && cmake .. && make && make format && make test
+echo
+echo CODEGEN OK

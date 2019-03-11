10842b17e372e33bae0093e384f562d957183124
Victor Grishchenko <gritzko@pm.me>
2019-03-12 00:57:33 +0300

    script: the commit routine

---
+++ bin/commit.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+DIR=`date +%H:%M:%S_%d-%m-%Y`
+
+if [ ! -d .git ]; then
+    echo please launch from the project\'s root
+    echo 1
+fi
+
+git pull || exit 9
+
+git diff $@ || exit 10
+
+pause
+
+git add $@ || exit 4
+
+git stash push --keep-index -m "$DIR" | grep WIP
+STASH=$?
+if [ $STASH ]; then
+    echo stashed $DIR
+    git stash show
+    pause
+fi
+
+mkdir $DIR || exit 2
+
+cd $DIR
+
+cmake .. || exit 3
+
+make || exit 5
+
+make test || exit 7
+
+make format
+
+cd ..
+
+git add $@ || exit 11
+
+git commit -S || exit 6
+
+#git push || exit 8
+
+if [ $STASH ]; then
+    git stash pop
+fi
+
+rm -rf $DIR
